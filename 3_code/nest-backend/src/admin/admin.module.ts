c0e9d4124932987144233f4eab93e2459b635f66
OzLynx <rickozz68@gmail.com>
2022-11-24 14:21:46 +0700

    error module fixes

--- 3_code/nest-backend/src/admin/admin.module.ts
+++ 3_code/nest-backend/src/admin/admin.module.ts
@@ -6,9 +6,13 @@ import { FaqService } from './master/faq/faq.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Faq } from 'src/typeorm/entities/Faq';
 import { Product } from 'src/typeorm/entities/Product';
+import { ProductOption } from 'src/typeorm/entities/ProductOption';
+import { ProductOptionValue } from 'src/typeorm/entities/ProductOptionValue';
+import { ProductInventory } from 'src/typeorm/entities/ProductInventory';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Faq, Product])],
+  imports: [TypeOrmModule.forFeature([Faq, Product, ProductOption, ProductOptionValue, ProductInventory])],
+  exports: [ProductService],
   controllers: [ProductController, FaqController],
   providers: [ProductService, FaqService]
 })
--- 3_code/nest-backend/src/page/page.module.ts
+++ 3_code/nest-backend/src/page/page.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PageProp } from 'src/typeorm/entities/PageProp';
+import { Product } from 'src/typeorm/entities/Product';
 import { PageController } from './controllers/page/page.controller';
 import { PageService } from './services/page/page.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PageProp, Product])],
   controllers: [PageController],
   providers: [PageService]
 })
