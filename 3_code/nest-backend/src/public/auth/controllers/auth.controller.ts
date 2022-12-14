d8412e5160025e7847f81967b1351a8b44029431
avonleira <71450408+avonleira@users.noreply.github.com>
2022-12-14 18:29:33 +0700

    Update auth.controller.ts

--- 3_code/nest-backend/src/public/auth/controllers/auth.controller.ts
+++ 3_code/nest-backend/src/public/auth/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Get, UseGuards, ValidationPipe, UsePipes, Body, UseInterceptors, ClassSerializerInterceptor, Res, Req } from '@nestjs/common';
-import { Request, response } from 'express';
+import { Request, Response } from 'express';
 import { GeneralSerialization } from 'src/general/GeneralSerialization';
 import { LoginDto } from '../dtos/Login.dto';
 import { RegisterDto } from '../dtos/Register.dto';
@@ -15,20 +15,36 @@ export class AuthController {
 
   }
 
+  @Get("coba")
+  async coba(@Req() req: Request) {
+    console.log(req.cookies)
+    return 0;
+  }
+
   @Post('login')
-  async login(@Body() loginDto: LoginDto, @Res({passthrough: true}) response, @Req() req: Request) {
+  async login(@Body() loginDto: LoginDto, @Res({passthrough: true}) response: Response, @Req() req: Request) {
     const result = await this.authService.login(loginDto)
     if (result) {
-      response.cookie('jwt', result.token, {httpOnly: true, domain: process.env.FRONTEND_DOMAIN,});
+      response.cookie('jwt', result.token, {httpOnly: true, domain: process.env.FRONTEND_DOMAIN, maxAge: (1000 * 60 * 60 * 24) });
     }
     return new GeneralSerialization(result);
   }
 
+  @Post('refresh')
+  async refresh(@Req() req: Request, @Res({passthrough: true}) response: Response) {
+    // const result = await this.authService.refresh()
+    // if (result) {
+    //   response.cookie('jwt', result.token, {httpOnly: true, domain: process.env.FRONTEND_DOMAIN, maxAge: (1000 * 60 * 60 * 24) });
+    // }
+    // return new GeneralSerialization(result);
+    return {token: "testoken"};
+  }
+
   @Post('register')
-  async register(@Body() registerDto: RegisterDto) {
+  async register(@Body() registerDto: RegisterDto, @Res({passthrough: true}) response: Response) {
     const result = await this.authService.register(registerDto)
     if (result) {
-      response.cookie('jwt', result.token, {httpOnly: true, domain: process.env.FRONTEND_DOMAIN,});
+      response.cookie('jwt', result.token, {httpOnly: true, domain: process.env.FRONTEND_DOMAIN, maxAge: (1000 * 60 * 60 * 24) });
     }
     return new GeneralSerialization(result);
   }
